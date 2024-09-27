088aacd083dbc509a9665f7ac22d9ae033b815fd
Jim <jimfreeze@gmail.com>
2024-09-27 11:44:45 -0500

    init

---
+++ lib/horizon.ex
@@ -0,0 +1,18 @@
+defmodule Horizon do
+  @moduledoc """
+  Documentation for `Horizon`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Horizon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
