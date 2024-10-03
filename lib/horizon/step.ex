32c83cfc2107b6e6d27c353110c21b5f65a28496
Jim <jimfreeze@gmail.com>
2024-10-03 12:24:12 -0500

    add steps

--- lib/horizon/step.ex
+++ lib/horizon/step.ex
@@ -1,15 +1,33 @@
 defmodule Horizon.Step do
+  @moduledoc """
+  The Horizon.Step module contains functions that are used to
+  perform specific tasks during the release process.
+  """
+
+  @doc """
+  Echo the release name and options to the console.
+  """
+  @spec echo(Mix.Release.t()) :: Mix.Release.t()
   def echo(%Mix.Release{name: name, options: options} = release) do
-    IO.puts("\u001b[32;1m  ===> running echo\u001b[0m")
-    dbg(name)
-    dbg(options)
+    IO.inspect(name: name, options: options)
     release
   end
 
-  def setup_rcd(%Mix.Release{name: name, options: options} = release) do
-    options_with_defaults = Horizon.Config.merge_defaults(options, name)
-    release = Map.put(release, :options, options_with_defaults)
+  @doc """
+  Merges the Horizon defaults into the release options.
+  """
+  @spec merge_defaults(Mix.Release.t()) :: Mix.Release.t()
+  def merge_defaults(%Mix.Release{name: name} = release) do
+    Map.update(release, :options, &Horizon.Config.merge_defaults(&1, name))
+    # options_with_defaults = Horizon.Config.merge_defaults(options, name)
+    # Map.put(release, :options, options_with_defaults)
+  end
 
+  @doc """
+  Create the rc.d script for the release.
+  """
+  @spec setup_rcd(Mix.Release.t()) :: Mix.Release.t()
+  def setup_rcd(%Mix.Release{name: name} = release) do
     file = "/usr/local/etc/rc.d/#{name}"
 
     # We don't overwrite the rc.d script.
