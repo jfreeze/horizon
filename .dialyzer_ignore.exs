4938bc01b46d07977fca4f3b64cc4ddb67a4d8e8
Jim <jimfreeze@gmail.com>
2024-10-15 16:43:10 -0500

    dialyzer doesn't like us referencing Mix.Release

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,3 +1,16 @@
 [
-  {"lib/horizon.ex", :unknown_function, 64}
+  {"lib/horizon.ex", :unknown_function, 64},
+  # Ignore all unknown functions related to Mix.shell/0 and Mix.Project.config/0
+  ~r"lib/horizon.ex:\d+:unknown_function Function Mix.shell/0 does not exist",
+  ~r"lib/horizon.ex:\d+:unknown_function Function Mix.Project.config/0 does not exist",
+  ~r"lib/mix/tasks/horizon/init.ex:\d+:unknown_function Function Mix.Project.config/0 does not exist",
+
+  # Ignore no_return warnings in horizon/step.ex
+  ~r"lib/horizon/step.ex:\d+:no_return",
+
+  # Ignore unknown type Mix.Release.t/0 warnings
+  ~r"lib/horizon/step.ex:\d+:\d+:unknown_type Unknown type: Mix.Release.t/0",
+
+  # Ignore callback info missing for Mix.Task behavior
+  ~r"lib/mix/tasks/horizon/init.ex:\d+:callback_info_missing"
 ]
