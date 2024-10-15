be48c700fc9944c78e3a02746986e769beda234c
Jim <jimfreeze@gmail.com>
2024-10-15 15:32:17 -0500

    add step tests

---
+++ test/horizon/step_test.exs
@@ -0,0 +1,112 @@
+defmodule Horizon.StepTest do
+  use ExUnit.Case
+  alias Horizon.Step
+
+  describe "merge_defaults/1" do
+    test "merges default values into the release" do
+      # Initial release struct with some values
+      release = %Mix.Release{
+        name: :my_app,
+        version: "1.0.0",
+        path: "/full/path/to/project/_build/prod/rel/my_app",
+        version_path: "/full/path/to/project/_build/prod/rel/my_app/releases/1.0.0"
+      }
+
+      # Expected result after applying defaults (update as per actual defaults)
+      expected_release = %Mix.Release{
+        name: :my_app,
+        version: "1.0.0",
+        path: "/usr/local/my_app",
+        version_path: "/usr/local/my_app/releases/1.0.0",
+        options: [
+          {:path, "/usr/local/my_app"},
+          {:bin_path, "bin"},
+          {:build_path, "/usr/local/opt/my_app/build"},
+          {:releases_path, ".releases"},
+          {:build_host, "HOSTUNKNOWN"},
+          {:build_user, "$(whoami)"}
+        ]
+      }
+
+      # Call the function
+      updated_release = Step.merge_defaults(release)
+
+      # Assert the expected defaults are merged
+      assert updated_release == expected_release
+    end
+
+    test "does not override existing values" do
+      release = %Mix.Release{
+        name: :my_app,
+        version: "1.0.0",
+        path: "_build/prod/rel/my_app",
+        version_path: "/full/path/_build/prod/rel/my_app/releases/1.0.0",
+        applications: [:my_custom_app]
+      }
+
+      expected_release = %Mix.Release{
+        name: :my_app,
+        version: "1.0.0",
+        path: "/usr/local/my_app",
+        version_path: "/usr/local/my_app/releases/1.0.0",
+        applications: [:my_custom_app],
+        boot_scripts: nil,
+        erts_source: nil,
+        erts_version: nil,
+        config_providers: nil,
+        options: [
+          path: "/usr/local/my_app",
+          bin_path: "bin",
+          build_path: "/usr/local/opt/my_app/build",
+          releases_path: ".releases",
+          build_host: "HOSTUNKNOWN",
+          build_user: "$(whoami)"
+        ],
+        overlays: nil,
+        steps: nil
+      }
+
+      updated_release = Step.merge_defaults(release)
+      assert updated_release == expected_release
+    end
+
+    test "user can override path" do
+      release = %Mix.Release{
+        name: :my_app,
+        version: "1.0.0",
+        path: "_build/prod/rel/my_app",
+        version_path: "/full/path/_build/prod/rel/my_app/releases/1.0.0",
+        applications: [:my_custom_app],
+        options: [
+          path: "/usr/phoenix/my_app",
+          bin_path: "bin_myapp"
+        ]
+      }
+
+      expected_release = %Mix.Release{
+        name: :my_app,
+        version: "1.0.0",
+        path: "/usr/phoenix/my_app",
+        version_path: "/usr/phoenix/my_app/releases/1.0.0",
+        applications: [:my_custom_app],
+        boot_scripts: nil,
+        erts_source: nil,
+        erts_version: nil,
+        config_providers: nil,
+        options: [
+          build_path: "/usr/local/opt/my_app/build",
+          releases_path: ".releases",
+          build_host: "HOSTUNKNOWN",
+          build_user: "$(whoami)",
+          path: "/usr/phoenix/my_app",
+          bin_path: "bin_myapp"
+        ],
+        overlays: nil,
+        steps: nil
+      }
+
+      updated_release = Step.merge_defaults(release)
+      assert updated_release == expected_release
+    end
+  end
+end
