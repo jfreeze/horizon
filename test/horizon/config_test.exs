fe12e9ed55d170020cd7f936f3d6308ffe06d3d3
Jim <jimfreeze@gmail.com>
2024-10-15 16:34:02 -0500

    fix tests

--- test/horizon/config_test.exs
+++ test/horizon/config_test.exs
@@ -3,84 +3,51 @@ defmodule Horizon.ConfigTest do
   alias Horizon.Config
 
   describe "merge_defaults/1" do
-    test "merges default values into the release" do
-      release = %Mix.Release{
-        name: :my_app,
-        version: "1.0.0",
-        path: "/some/full/path/my_app",
-        version_path: "some/full/path/1.0.0",
-        options: [
-          bin_path: "my_bin"
-        ]
-      }
+    test "merges default values into the release options" do
+      options = []
 
       expected_releases = [
-        my_app: %Mix.Release{
-          name: :my_app,
-          version: "1.0.0",
-          path: "/usr/local/my_app",
-          version_path: "/usr/local/my_app/releases/1.0.0",
-          applications: nil,
-          boot_scripts: nil,
-          erts_source: nil,
-          erts_version: nil,
-          config_providers: nil,
-          options: [
-            path: "/usr/local/my_app",
-            build_path: "/usr/local/opt/my_app/build",
-            releases_path: ".releases",
-            build_host: "HOSTUNKNOWN",
-            build_user: "$(whoami)",
-            bin_path: "my_bin"
-          ],
-          overlays: nil,
-          steps: nil
-        }
+        my_app: [
+          {:path, "/usr/local/my_app"},
+          {:bin_path, "bin"},
+          {:build_path, "/usr/local/opt/my_app/build"},
+          {:releases_path, ".releases"},
+          {:build_host, "HOSTUNKNOWN"},
+          {:build_user, "$(whoami)"}
+        ]
       ]
 
-      updated_releases = Config.merge_defaults(my_app: release)
+      updated_releases = Config.merge_defaults(my_app: options)
 
       assert updated_releases == expected_releases
     end
 
     test "uses user provided path values" do
-      release = %Mix.Release{
+      options = [
         name: :my_app,
         version: "1.0.0",
-        path: "_build/prod/rel/my_app",
-        applications: [:my_custom_app],
-        options: [
-          path: "/my/deploys/my_app"
-        ]
-      }
-
-      expected_releases = [
-        my_app: %Mix.Release{
-          name: :my_app,
-          version: "1.0.0",
-          path: "/my/deploys/my_app",
-          version_path: "/my/deploys/my_app/releases/1.0.0",
-          applications: [:my_custom_app],
-          boot_scripts: nil,
-          erts_source: nil,
-          erts_version: nil,
-          config_providers: nil,
-          options: [
-            bin_path: "bin",
-            build_path: "/usr/local/opt/my_app/build",
-            releases_path: ".releases",
-            build_host: "HOSTUNKNOWN",
-            build_user: "$(whoami)",
-            path: "/my/deploys/my_app"
-          ],
-          overlays: nil,
-          steps: nil
-        }
+        path: "/some/full/path/my_app",
+        version_path: "some/full/path/1.0.0",
+        bin_path: "my_bin"
       ]
 
-      updated_release = Horizon.Config.merge_defaults(my_app: release)
+      expected_releases = [
+        my_app: [
+          {:build_path, "/usr/local/opt/my_app/build"},
+          {:releases_path, ".releases"},
+          {:build_host, "HOSTUNKNOWN"},
+          {:build_user, "$(whoami)"},
+          {:name, :my_app},
+          {:version, "1.0.0"},
+          {:path, "/some/full/path/my_app"},
+          {:version_path, "some/full/path/1.0.0"},
+          {:bin_path, "my_bin"}
+        ]
+      ]
 
-      assert updated_release == expected_releases
+      updated_releases = Config.merge_defaults(my_app: options)
+
+      assert updated_releases == expected_releases
     end
   end
 end
