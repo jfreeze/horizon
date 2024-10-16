f917fd22ade79b4fbc8c0da78d23ba951094d50c
Jim <jimfreeze@gmail.com>
2024-10-15 21:45:34 -0500

    change dep

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Horizon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.34.2", runtime: false},
+      {:ex_doc, "~> 0.34", runtime: false},
       {:dialyxir, "~> 1.4", runtime: false}
     ]
   end
