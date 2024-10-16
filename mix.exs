ede999d4b80f6ba61d102fdbf5ecfbd88d40dcec
Jim <jimfreeze@gmail.com>
2024-10-15 21:46:45 -0500

    back off elixir requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Horizon.MixProject do
     [
       app: :horizon,
       version: "0.1.0",
-      elixir: "~> 1.17.3",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: [
