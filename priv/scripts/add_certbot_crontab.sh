b44bf9cee8d7e28ab48ea4f628243b5d9ff64d38
Jim <jimfreeze@gmail.com>
2024-11-06 23:39:31 -0600

    combine user@host into single arg

--- priv/scripts/add_certbot_crontab.sh
+++ priv/scripts/add_certbot_crontab.sh
@@ -2,26 +2,10 @@
 
 # Script to add certbot renew command to crontab
 
-USER=$(whoami)
-
-# Parse options
-while getopts ":u:" opt; do
-  case $opt in
-  u)
-    USER="$OPTARG"
-    ;;
-  \?)
-    echo "Invalid option: -$OPTARG" >&2
-    echo "Usage: $0 [-u user] host"
-    exit 1
-    ;;
-  esac
-done
-
-shift $((OPTIND - 1))
-
-if [ $# -lt 1 ]; then
-  echo "Usage: $0 [-u user] host"
+# Check arguments
+if [ $# -eq 1 ]; then
+  echo "Invalid option: -$OPTARG" >&2
+  echo "Usage: $0 host"
   exit 1
 fi
 
@@ -31,7 +15,7 @@ HOST="$1"
 CRON_JOB="0 0 * * 0 doas /usr/local/bin/certbot renew --quiet"
 
 # SSH into the remote host and execute the commands
-ssh "$USER@$HOST" /bin/sh <<EOF
+ssh "$HOST" /bin/sh <<EOF
 # Check if the cron job already exists in the crontab
 CRON_JOB='$CRON_JOB'
 crontab -l 2>/dev/null | grep -F "\$CRON_JOB" >/dev/null 2>&1
